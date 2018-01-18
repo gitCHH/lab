e4f0072c7094480646a0c9ca0d14812968f39a88
gitCHH <1572895048@qq.com>
2018-01-18 16:24:58 +0800

    add completeness check

--- chai-statistics.py
+++ chai-statistics.py
@@ -1,8 +1,24 @@
+
 n_groups = 5
 n_each = 4
 n_sections = n_each * n_groups + 1
 
-m5out_dir = 'D:/'
+
+def check_complete():
+    i = 0
+    for line in open(m5out_dir + 'system.pc.com_1.terminal', 'r'):
+        if line.find('m5 end') >= 0:
+            i = i + 1
+    print(i)
+    if i < n_groups:
+        print('warning: groups less than ', n_groups, ', need another ', n_groups - i, ' group(s)')
+    i = 0
+    for line in open(m5out_dir + 'stats.txt', 'r'):
+        if line.find('sim_seconds') >= 0:
+            i = i + 1
+    print(i)
+    if i < n_sections:
+        print('warning: sections less than ', n_sections, ',  need another ', n_sections - i, ' section(s)')
 
 
 def get_runtime():
@@ -20,7 +36,9 @@ def get_runtime():
                 runtimes.append(line.split()[1])
             elif i > n_sections:
                 print('warning: section number is not correct.')
-    print('base time: ', round(float(base_runtime)*1000,3), 'ms, finish time: ', round(float(finish_runtime)*1000,3), 'ms, (finish - base)/n_groups: ',round((float(finish_runtime) - float(base_runtime)) / n_groups*1000,3),'ms')
+    print('base time: ', round(float(base_runtime) * 1000, 3), 'ms, finish time: ',
+          round(float(finish_runtime) * 1000, 3), 'ms, (finish - base)/n_groups: ',
+          round((float(finish_runtime) - float(base_runtime)) / n_groups * 1000, 3), 'ms')
     return runtimes
 
 
@@ -62,5 +80,12 @@ def get_PU_runtime():
         PU_times[n_each])) / 2 * 1000
     print('time of each iteration: ', round(iteration_time, 3), 'ms')
 
-
-get_PU_runtime()
+# HSTO 0 need rerun, BS need run
+benchmark = 'SC'
+ratios = ['0','2', '4', '6', '8', '10', '12', '14', '16', '18', '20']
+for percent in ratios:
+    m5out_dir = 'D:/m5out2t_on_8core/'
+    m5out_dir = m5out_dir + benchmark + percent + '/'
+    print('----------------benchmark: ', benchmark, ', percent: ', percent, '%-----------------------')
+    check_complete()
+    # get_PU_runtime()

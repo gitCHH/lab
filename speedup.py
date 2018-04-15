9f6e04bd7f24e8bba0c8569c2313e6b91ddaf85b
gitCHH <1572895048@qq.com>
2018-04-15 16:34:34 +0800

    two table in one Figure

--- speedup.py
+++ speedup.py
@@ -13,7 +13,7 @@ hatch = patterns[0]
 
 def f(gpu_arch='fermi', grain=5):
     m5out_dir_home = 'D:/6TTiming/'
-    bench_initRatio = { 'bs':10,'cedd': 20, 'rscd': 20}
+    bench_initRatio = {'bs': 10, 'cedd': 20,'rscd': 20}
     GPU_heights = []
     Init_heights = []
     Tuned_heights = []
@@ -37,26 +37,29 @@ def f(gpu_arch='fermi', grain=5):
         GPU_heights.append(max(time_all_GPU))
         Init_heights.append(max(init_CG_time))
         Tuned_heights.append(max(CG_time))
+    print('Fermi GPU',GPU_heights)
+    print('Init',Init_heights)
+    print('Tuned',Tuned_heights)
 
     fig, axs = plt.subplots(1, 2, figsize=(4, 6), frameon=False)
     ax = axs[0]
     ax.set_xticklabels(bench_initRatio.keys())
     index = np.arange(1, len(bench_initRatio) + 1)
-    bar_width = 0.15
-    opacity = 0.2
+    bar_width = 0.28
+    opacity = 0.3
     ax.set_xticks(index)
-    ax.set_title(bench)
+    ax.set_title('Fermi GPU')
     ax.set_ylabel('Execution Time(ms)')
-    ax.set_xlabel('Fermi GPU')
-
+    ax.set_xlabel('Benchmarks')
     ax.bar(index - bar_width, GPU_heights, bar_width, hatch='.', alpha=opacity, color='black', label='GPU')
-    ax.bar(index, Init_heights, bar_width, hatch='/', alpha=opacity, color='black', label='init')
-    ax.bar(index+bar_width, Tuned_heights, bar_width, hatch='|', alpha=opacity, color='black', label='tuned')
+    ax.bar(index, Init_heights, bar_width, hatch='//', alpha=opacity, color='black', label='init')
+    ax.bar(index + bar_width, Tuned_heights, bar_width, hatch='\\\\', alpha=opacity, color='black', label='tuned')
     ax.legend()
+
     GPU_heights.clear()
     Init_heights.clear()
     Tuned_heights.clear()
-    gpu_arch='maxwell'
+    gpu_arch = 'maxwell'
     for bench in bench_initRatio.keys():
         m5out_dir = m5out_dir_home + bench + '/' + gpu_arch + '_m5out/' + bench + str(
             bench_initRatio[bench]) + '/'
@@ -77,20 +80,22 @@ def f(gpu_arch='fermi', grain=5):
         GPU_heights.append(max(time_all_GPU))
         Init_heights.append(max(init_CG_time))
         Tuned_heights.append(max(CG_time))
+    print('Maxwell GPU', GPU_heights)
+    print('Init', Init_heights)
+    print('Tuned', Tuned_heights)
     ax = axs[1]
     ax.set_xticklabels(bench_initRatio.keys())
     index = np.arange(1, len(bench_initRatio) + 1)
-    bar_width = 0.15
-    opacity = 0.2
     ax.set_xticks(index)
-    ax.set_title(bench)
+    ax.set_title('Maxwell GPU')
     ax.set_ylabel('Execution Time(ms)')
-    ax.set_xlabel('Maxwell GPU')
-
+    ax.set_xlabel('Benchmarks')
     ax.bar(index - bar_width, GPU_heights, bar_width, hatch='.', alpha=opacity, color='black', label='GPU')
-    ax.bar(index, Init_heights, bar_width, hatch='/', alpha=opacity, color='black', label='init')
-    ax.bar(index + bar_width, Tuned_heights, bar_width, hatch='|', alpha=opacity, color='black', label='tuned')
+    ax.bar(index, Init_heights, bar_width, hatch='//', alpha=opacity, color='black', label='init')
+    ax.bar(index + bar_width, Tuned_heights, bar_width, hatch='\\\\', alpha=opacity, color='black', label='tuned')
     ax.legend()
     fig.tight_layout()
     plt.show()
+
+
 f()

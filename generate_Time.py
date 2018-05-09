eaad169b1591ef776fbd9c25e5d2121f5f036a17
gitCHH <1572895048@qq.com>
2018-05-09 14:41:28 +0800

    correct algorithm, add print

--- generate_Time.py
+++ generate_Time.py
@@ -12,6 +12,10 @@ def profile_cal(queue_size, v_c, v_g, init_size, grain):
     '''
     time = 0
     time = time + max(v_c, v_g) * init_size
+    if v_c>v_g:
+        print('vc>vg(time/data) wait time: ',time-v_g*init_size)
+    else:
+        print('vg>vc(time/data) wait time: ',time-v_c*init_size)
     new_ratio_remain = int(v_g / (v_c + v_g) * (100 - 2 * init_size))
     if new_ratio_remain % grain != 0:
         if v_g > v_c:
@@ -23,7 +27,7 @@ def profile_cal(queue_size, v_c, v_g, init_size, grain):
 
 def share_cal(queue_size, v_c, v_g, grain):
     '''
-
+    数据块作为双端队列
     :param queue_size: 数据总量
     :param v_c: 单位是 时间每1%数据量
     :param v_g:
@@ -31,7 +35,24 @@ def share_cal(queue_size, v_c, v_g, grain):
     :return: 总时间
     '''
     time = 0
-    new_ratio = v_c / (v_c + v_g)
+    new_ratio = int(v_g / (v_c + v_g))
+    if v_c>v_g:
+        print('vc>vg(time/data)')
+    else:
+        print('vg>vc(time/data)')
     remainder = new_ratio % grain
-    if new_ratio % grain != 0:
-        gpu_ratio = 100 - new_ratio - (100 - new_ratio) * (remainder / new_ratio)
+    if remainder != 0:
+        new_ratio=new_ratio-remainder
+        gpu_ratio = 100-new_ratio-grain
+    c_elapse=v_c*new_ratio
+    g_elapse=v_g*gpu_ratio
+    if c_elapse>g_elapse:
+        print('last is gpu: ',v_g*grain)
+        return time+max(c_elapse,g_elapse)+v_g*grain
+    else:
+        print('last is cpu: ',v_c*grain)
+        return time+max(c_elapse,g_elapse)+v_c*grain
+
+def aware(queue_size,v_c,c_g,grain):
+
+
